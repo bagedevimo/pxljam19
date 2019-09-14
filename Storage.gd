8f2821630c677d1c52d6a33fd996a78fc8f61403
Erik Fercak <erik.fercak@gmail.com>
2019-09-14 20:58:48 +1200

    Add storage

---
+++ Storage.gd
@@ -0,0 +1,44 @@
+# Usage:
+# $Storage.store("logs", 10)
+# 
+# $Storage.has("logs") // true
+# $Storage.has("logs", 12) // false
+# $Storage.has("logs", 5) // true
+#
+# $Storage.retrieve("logs", 4) // 4
+# $Storage.retrieve("logs", 7) // false
+# $Storage.retrieve("logs") // 6
+
+extends Node2D
+
+var storage = {}
+
+func store(key, amount):
+	if !storage.has(key):
+		storage[key] = amount
+	else:
+		storage[key] += amount
+
+func retrieve(key, amount = null):
+	if !has(key, amount):
+		return false
+
+	if amount == null:
+		var everything = storage[key]
+		storage[key] = 0
+		return  everything
+
+	storage[key] -= amount
+	return amount
+
+func has(key, amount = null):
+	if !storage.has(key):
+		return false
+
+	if amount == null:
+		return true
+
+	if storage[key] < amount:
+		return false
+	else:
+		return true
