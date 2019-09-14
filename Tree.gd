69d822a022d8c75ab99c3abd0c017c9192ec90a7
Ben Anderson <benanderson@acidic.co.nz>
2019-09-14 21:00:17 +1200

    Fallen trees transform into logs

--- Tree.gd
+++ Tree.gd
@@ -3,6 +3,8 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export var requiredInteracts = 5
+var interacts = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +16,11 @@ func _ready():
 
 func interact():
 	$AnimationPlayer.play("Shake")
+	
+	self.interacts += 1
+	
+	if self.interacts >= self.requiredInteracts:
+		var logs = load("res://Logs.tscn").instance()
+		logs.position = self.position
+		get_tree().get_root().add_child(logs)
+		self.queue_free()
