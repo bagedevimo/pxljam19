c0afea37a248e9ea367daa4fd9e7a3d643f39273
Ben Anderson <benanderson@acidic.co.nz>
2019-09-14 17:38:29 +1200

    Add pings, add support to interactableS

--- Interactable.gd
+++ Interactable.gd
@@ -16,8 +16,10 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
 		body.set_focus(get_parent())
+		$Attention.visible = true
 
 
 func _on_Area2D_body_exited(body):
 	if body.is_in_group("player"):
 		body.clear_focus_where(get_parent())
+		$Attention.visible = false
